e77b87381b3b5e5ee725e379b61ff78b8ec93dfa
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-12 19:35:24 -0300

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,29 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+/* reset css */
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
   }
-`
+  #_next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +33,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
